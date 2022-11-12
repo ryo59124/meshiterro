2dd121ab7f375e271781d3ff38f4e0419470c4d9
EC2 Default User <117144349+ryo59124@users.noreply.github.com>
2022-11-12 07:16:39 +0000

    4-17fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
   devise_for :users
   
+  root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
   #get 'meshiterro/new'
   
   get 'homes/about', to: 'homes#about', as: 'about'
